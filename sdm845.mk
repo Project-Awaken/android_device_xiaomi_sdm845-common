807668a5c55333a28090ac363e2ea9f1964843aa
Signed-off-by: Erfan Abdi <erfangplus@gmail.com>
2020-09-09 19:45:10 +0200

    sdm845-common: Refactor BoardConfigQcom to ProductConfigQcom
    Change-Id: Iddbc6ae3e21cebab3f55b4907abc08527523b81a

--- sdm845.mk
+++ sdm845.mk
@@ -14,6 +14,10 @@ $(call inherit-product-if-exists, vendor/xiaomi/sdm845-common/sdm845-common-vend
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Board
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_BOARD_PLATFORM := sdm845
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
